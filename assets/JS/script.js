4dc3439c52c04b9166c3226e11432ac0b3e9ec71
dembasy <dembasy1227@gmail.com>
2018-06-25 14:23:08 +0200

    correction exercice 6

---
+++ assets/JS/script.js
@@ -0,0 +1,6 @@
+function imgWrk(id){
+document.getElementById(id).src = 'assets/IMG/' + id + '_2.jpg';
+}
+function reverseWrk(id){
+  document.getElementById(id).src = 'assets/IMG/' + id + '.jpg';
+}
